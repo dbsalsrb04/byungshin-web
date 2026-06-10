ab36dde9ff2a16eaa52bf1309965da7bfa648943
dbsalsrb04 <qw346125@gmail.com>
2026-06-11 02:01:57 +0900

    클라이밍 차트 툴팁 서식 변경 & 주석 추가

--- mingyu/climbing/climbing.js
+++ mingyu/climbing/climbing.js
@@ -146,11 +146,28 @@ function apply_saved_columns() {
 }
 
 // 차트 그리기
-// 모든 난이도 완등률 차트
-// 함수 내에서 데이터 가공하는 게 더 나을 듯
 // x축은 방문한 모든 날짜.
+/* === 설명 ===
+1. successRateChart(태그 id, 클라이밍 데이터 원본)
+2. [날짜(x축)|완등률|시도문제 수|완등문제 수] 정의
+3. 더클라임 아닌 지역 제외, for문으로 success_rates 배열에 각 날짜마다
+   limbing_data의 데이터를 가공하여 완등률만 push.
+   tries_count와 successes_count는 이 for문에서 같이 시도문제수|완등문제수만 넣음
+   따로 하면 더클라임 아닌 지역도 포함되어서, 날짜와 맞지 않는 경우 발생
+4. for문을 사용하여 chart 호출 시 datasets에 넣을 값을 미리 만들어놓기
+   chart_dataset 배열을 미리 정의하여 이 배열에 chart 호출 시 넣을 값과 동일하게 만듦.
+   대신 tries_counts와 successes_counts도 같이 넣음. 얘네 2개 넣는다고 문제 안됨.
+   이러면 나중에 차트 포인터(점)에 갖다댔을 때 실제 문제 수도 같이 볼 수 있게 할 수 있음.
+5. chart 함수 호출. plugins: {tooltip: {... callbacks: { ... } ... } ... }
+   위 callbacks 에서 드디어 tries_counts와 successes_counts를 사용.
+   이로써 툴팁에서 실제 시도문제 수와 완등문제 수를 볼 수 있게 됨.
+
++ spots도 동일한 방법으로..
+*/
 function successRateChart(canvas_id, climbing_data) {
     let dates = [];
+    let spots = [];
+
     let success_rates = {
          black: [],
          brown: [],
@@ -163,35 +180,72 @@ function successRateChart(canvas_id, climbing_data) {
         orange: [],
         yellow: [],
          white: []
-    }
+    };
+    let tries_counts = {
+        black: [],
+        brown: [],
+         gray: [],
+       purple: [],
+         pink: [],
+          red: [],
+         blue: [],
+        green: [],
+       orange: [],
+       yellow: [],
+        white: []
+    };
+    let successes_counts = {
+        black: [],
+        brown: [],
+         gray: [],
+       purple: [],
+         pink: [],
+          red: [],
+         blue: [],
+        green: [],
+       orange: [],
+       yellow: [],
+        white: []
+    };
 
     const chart_dataset = [];
     
+    // success_rates|tries_counts|successes_counts에 데이터 넣기
     for (const record of climbing_data) {
         // 더클라임 외 지점은 제외함
         if (record.spot.includes("더클라임")) {
-            dates.push(`${record.spot}\n${record.date}`);
+            dates.push(record.date); // 날짜
+            spots.push(record.spot); // 지점
             // 완등률 구하기
-            // rate_color: success_rates의 각 색깔 리스트
+            // color: success_rates의 각 색깔 리스트
             // climbing_data의 tries와 successes 의 key 이름이 똑같음.
-            // record.successes[rate_color] => data에서 현재 가리키고 있는 방문일의 특정 난이도(=색깔)의 완등 개수
-            for (const rate_color in success_rates) {
-                let rate = Number((record.successes[rate_color] / record.tries[rate_color] * 100).toFixed(1));
-                success_rates[rate_color].push(rate)
+            // record.successes[color] => data에서 현재 가리키고 있는 방문일의 특정 난이도(=색깔)의 완등 개수
+            for (const color in success_rates) {
+                let rate = Number((record.successes[color] / record.tries[color] * 100).toFixed(1));
+                success_rates[color].push(rate);
+
+                // 이 2개 => 성공|완등 문제 수 미리 넣기
+                tries_counts[color].push(record.tries[color]);
+                successes_counts[color].push(record.successes[color]);
             }
         }
-        
     }
 
     // chart의 data 속성 중 datasets 속성에 넣을 값 미리 넣기
-    for (const rate_color in success_rates) {
+    for (const color in success_rates) {
         chart_dataset.push({ // 데이터 속성
-            label: rate_color, // 데이터 제목
-            data: success_rates[rate_color], // 데이터
+            label: color, // 데이터 제목
+            data: success_rates[color], // 데이터
+
+            // dataset 안에 지점/완등/성공 문제 수도 같이 넣어놓기
+            spots: spots,
+            tries: tries_counts[color],
+            successes: successes_counts[color],
+
             pointRadius: 3.5,
             pointHoverRadius: 7,
-            backgroundColor: COLORS[rate_color],
-            borderColor: COLORS[rate_color],
+            backgroundColor: COLORS[color],
+            borderColor: COLORS[color],
             // pointBackgroundColor: 'transparent', // 포인터 배경 없애기
             // pointBorderWidth: 3 // 포인터 테두리 굵기
             // borderWidth: 2.5
@@ -219,16 +273,51 @@ function successRateChart(canvas_id, climbing_data) {
             },
             // 플러그인
             plugins: {
-                tooltip: { // 툴팁
+                // 툴팁
+                tooltip: {
                     enabled: true, // 툴팁 활성화 (기본값 true)
                     // intersect: true, // ?
                     mode: 'index', // 툴팁 팝업에서 그 x축에 대한 모든 y축 정보.
                     backgroundColor: '#808080', // 툴팁 색상
                     padding: 10, // 툴팁 패딩
+                    // 타이틀 폰트
+                    titleFont: {
+                        family: "consolas"
+                    },
+                    // 내용 폰트
+                    bodyFont: {
+                        family: "consolas"
+                    },
                     callbacks: {
+                        // 타이틀 텍스트 변경
+                        /* === title 설명 ===
+                            1. items로 해당 x축의 색깔(에 대한 데이터)이 들어옴.
+                            2. 만약 그날 파랑&빨강 의 기록이 있었다면
+                               items는 길이가 2인 배열{{파랑 데이터}, {빨강 데이터}} 임.
+                            3. spots 배열은 {'양재', '성수', ... } 의 형태이기 때문에,
+                               key가 아닌 인덱스가 반드시 필요.
+                               items의 요소들은 전부 다 dataIndex를 갖고 있고,
+                               그것이 같은 items 배열로 들어왔다면, 전부 같은 값임.
+                               예를 들어, 위의 items에서, 파랑 데이터와 빨강 데이터 배열의
+                               dataIndex: (같은 값) 일 것.
+                            4. (3)을 이용하여 items[0].dataIndex로 미리 인덱스를 구함.
+                               items[1] 로 해도 될테지만, items[0]은 항상 존재함이
+                               보장되기 때문에 그냥 items[0]으로 함.
+                               이렇게 해서 item = items[0] 으로 정의.
+                            5. 어쨌든, 이렇게 구한 인덱스는 idx로 정의하고,
+                               아래와 같이 return~
+                        */
+                        title: function(items) {
+                            const item = items[0];
+                            const idx = item.dataIndex;
+                            return `${item.dataset.spots[idx]}\n${item.label}`;
+                        },
                         // 레이블 텍스트 변경
                         label: function(ctx) {
-                            return ctx.dataset.label + ": " + ctx.raw + "%";
+                            // console.log(ctx);
+                            const label = ctx.dataset.label.padEnd(6, ' ');
+                            const rate  = `${ctx.raw}`.padStart(4, ' ');
+                            return `${label}: ${rate}% (${ctx.dataset.successes[ctx.dataIndex]}/${ctx.dataset.tries[ctx.dataIndex]})`;
                         }
                     }
                 },
@@ -261,8 +350,8 @@ function successRateChart(canvas_id, climbing_data) {
                         }
                     },
                     // min: 61, // y축 최솟값
-                    // max: 64, // y축 최댓값
-                    border: { dash: [0,0] } // 점선
+                    // max: 100, // y축 최댓값
+                    // border: { dash: [0,0] } // 점선
                 }
             }
             // title: {
