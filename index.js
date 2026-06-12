f095e1dd76393b2a1d067d7e944d7d0716c5f6f1
dbsalsrb04 <qw346125@gmail.com>
2026-06-12 16:28:01 +0900

    메인화면 애니메이션 추가 & 새 페이지 추가

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+document.querySelectorAll(".spinning-mingyu").forEach(img => {
+    img.addEventListener("click", function() {
+        this.classList.add("dead");
+        // animationend: CSS 애니메이션 끝났을 때 발생하는 이벤트
+        this.addEventListener("animationend", () => {
+            this.parentElement.remove();
+        }, { once: true })
+    });
+});
+
 function resolution() {
     console.log(`${screen.width} * ${screen.height}`)
     document.getElementById("resolution").innerText = `해상도: ${screen.width} * ${screen.height}`;
